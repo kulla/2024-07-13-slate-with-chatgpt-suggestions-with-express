04f36402555c07e22da1f8d3631a55f26725bc17
Stephan Kulla <git.mail@kulla.me>
2024-07-13 20:09:17 +0200

    feat: Add react frontend

--- backend/index.ts
+++ backend/index.ts
@@ -1,3 +1,4 @@
+import esbuild from 'esbuild'
 import express from 'express'
 import { dirname, join } from 'path'
 import { fileURLToPath } from 'url'
@@ -5,6 +6,26 @@ import { fileURLToPath } from 'url'
 const __filename = fileURLToPath(import.meta.url)
 const projectDir = dirname(dirname(__filename))
 const app = express()
+
+app.get('/index.js', async (_, res) => {
+  res.setHeader('Content-Type', 'application/javascript')
+
+  try {
+    const result = await esbuild.build({
+      entryPoints: [join(projectDir, 'frontend', 'index.tsx')],
+      bundle: true,
+      format: 'esm',
+      platform: 'browser',
+      write: false,
+    })
+
+    res.setHeader('Content-Type', 'application/javascript')
+    res.send(result.outputFiles[0].text)
+  } catch (error) {
+    console.error('Error bundling TypeScript:', error)
+    res.status(500).send('Error bundling TypeScript')
+  }
+})
 
 app.get('/', (_, res) => {
   res.sendFile(join(projectDir, 'public', 'index.html'))
