61314235caab6cb3620f128cafb433b84be2b988
Stephan Kulla <git.mail@kulla.me>
2024-07-13 20:03:43 +0200

    feat: Add public/index.html

--- backend/index.ts
+++ backend/index.ts
@@ -1,9 +1,13 @@
 import express from 'express'
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
 
+const __filename = fileURLToPath(import.meta.url)
+const projectDir = dirname(dirname(__filename))
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
+app.get('/', (_, res) => {
+  res.sendFile(join(projectDir, 'public', 'index.html'))
 })
 
 app.listen(3000, () => {
